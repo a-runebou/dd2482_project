e290ab05a777872da196fd56ab344fb4151c6d43
Alexander Runebou <alexanderrunebou02@gmail.com>
2026-09-09 13:10:23 +0200

    feat: configure PostgreSQL and SQLAlchemy

--- backend/app/infra/db.py
+++ backend/app/infra/db.py
@@ -1,5 +1,7 @@
+from collections.abc import Generator
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 from app.config import get_settings
 
@@ -18,3 +20,11 @@ SessionLocal = sessionmaker(
     autoflush=False,
     expire_on_commit=False,
 )
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
