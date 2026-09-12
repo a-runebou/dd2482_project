013366f0bf83d3d97f67f926e98ccf7278b7a2f5
Alexander Runebou <alexanderrunebou02@gmail.com>
2026-09-13 00:56:13 +0200

    chore: linting

--- backend/alembic/versions/bcb51f836df7_add_user_notification_preference.py
+++ backend/alembic/versions/bcb51f836df7_add_user_notification_preference.py
@@ -5,17 +5,17 @@ Revises: 02daa394a268
 Create Date: 2026-09-13 00:54:05.237571
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'bcb51f836df7'
-down_revision: Union[str, Sequence[str], None] = '02daa394a268'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = '02daa394a268'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
