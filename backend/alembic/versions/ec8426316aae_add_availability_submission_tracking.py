1c77649462a1f725aae0261d92f37df6a0de6f68
Alexander Runebou <alexanderrunebou02@gmail.com>
2026-09-13 01:35:25 +0200

    chore: add availability submission tracking

--- backend/alembic/versions/ec8426316aae_add_availability_submission_tracking.py
+++ backend/alembic/versions/ec8426316aae_add_availability_submission_tracking.py
@@ -5,17 +5,17 @@ Revises: bcb51f836df7
 Create Date: 2026-09-13 01:33:52.381584
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'ec8426316aae'
-down_revision: Union[str, Sequence[str], None] = 'bcb51f836df7'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = 'bcb51f836df7'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
